fe283b91640798fa685f1d03730df6b74f2a4428
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-02 16:28:16 +0530

    Added: Storing daily wage along with total wage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,4 +1,5 @@
 echo "Welcome to Employee Wage Computation Program"
+declare -a dailyWage
 isPartTimePresent=2
 isFullTimePresent=1
 isAbsent=0
@@ -7,6 +8,7 @@ workingDays=20
 maxHrsInMonth=100
 monthSalary=0
 totalWorkingHr=0
+
 function getWorkingHours() {
 	case $empCheck in
 		$isFullTimePresent)
@@ -26,7 +28,9 @@ for (( i=0; i<$workingDays && $totalWorkingHr<$maxHrsInMonth; i++ ))
 do
 	empCheck=$((RANDOM%3))
 	workingHr="$( getWorkingHours $empCheck )"
+	dailyWage[i]=$((workingHr*wagePerHour))
 	totalWorkingHr=$((totalWorkingHr+workingHr))
 done
+echo "Employee Daily wage : ${dailyWage[@]}"
 monthSalary=$((wagePerHour*totalWorkingHr))
 echo "Employee monthly wage : $monthSalary"
