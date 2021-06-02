7b51b69b5ab4c24e763925c33aad2f62d6c0d72e
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-02 15:33:20 +0530

    Added: Employee Present or not

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,1 +1,20 @@
 echo "Welcome to Employee Wage Computation Program"
+present=1
+absent=0
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq $present ]
+then
+	echo "Employee present"
+else
+	echo "Employee absent"
+fi
+
+
+
+
+
+
+
+
+
+
