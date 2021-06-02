2c371a5fe3f70426a6fac50f8eecc6871454ec39
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-02 15:40:45 +0530

    Added: Employee daily wage program

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,20 +1,16 @@
 echo "Welcome to Employee Wage Computation Program"
 present=1
 absent=0
+wagePerHour=20
+workingHr=0
 empCheck=$((RANDOM%2))
 if [ $empCheck -eq $present ]
 then
 	echo "Employee present"
+	workingHr=8
 else
 	echo "Employee absent"
+	workingHr=0
 fi
-
-
-
-
-
-
-
-
-
-
+salary=$((wagePerHour*workingHr))
+echo "Daily employee wage : $salary"
